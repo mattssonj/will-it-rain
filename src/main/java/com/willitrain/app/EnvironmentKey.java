b6a8fce38df30a5b2e899c4706c7f1bf5851f79a
Joakim Mattsson <joamat91@gmail.com>
2018-06-10 19:21:42 +0200

    fixed problem with POM

--- src/main/java/com/willitrain/app/EnvironmentKey.java
+++ src/main/java/com/willitrain/app/EnvironmentKey.java
@@ -1,5 +1,14 @@
 package com.willitrain.app;
 
+/**
+ * This class is used to add environment variables.
+ * Should be salted
+ *
+ * The api key will be removed from OWM in a couple of weeks.
+ *
+ * After that anyone cloning the project need to add their own key in this file.
+ */
+
 public class EnvironmentKey {
 
     public static final String OWM_API_KEY = "a3bdc9339482378aee586a9de5ff1cad";
